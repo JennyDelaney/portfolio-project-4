aafc2b0bda78464ecadfb7d9847288447b7418db
Jenny Delaney <jennyelewis29@gmail.com>
2023-02-20 19:40:18 +0000

    Initial Set-up of Django, Heroku and database

--- fix_it_physio/settings.py
+++ fix_it_physio/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'fix_it_physio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))

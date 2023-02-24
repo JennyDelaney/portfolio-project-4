b08dc2960a2859b406af567a96e48bcf130222aa
Jenny Delaney <jennyelewis29@gmail.com>
2023-02-24 19:02:42 +0000

    Add Booking URL

--- fix_it_physio/urls.py
+++ fix_it_physio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booking_app.views import booking
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('booking/', booking, name='booking'),
 ]

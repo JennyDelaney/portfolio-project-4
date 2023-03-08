ce0692cc451a9672b7803034bbf3d75fef3fd9de
Jenny Delaney <jennyelewis29@gmail.com>
2023-03-08 20:41:53 +0000

    Fix base.html file to show properly

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
     path('book_appt/', views.BookAppointment.as_view(), name='book_appointment'),
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Booking
 
+
 class BookAppointment(generic.ListView):
     model = Booking
     queryset = Booking.objects.filter(status=1).order_by('-date_requested')
